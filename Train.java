9addc823033cde43194f7ca89d45e5e26cfdaeb6
ROKATG0D <58750536+ROKATG0D@users.noreply.github.com>
2021-05-04 14:15:46 +0200

    Commencement Train

--- Train.java
+++ Train.java
@@ -1,1 +1,11 @@
+import java.util.ArrayList;
 
+public class Train {
+	private int NB_WAGONS; 
+	private ArrayList<Bandit> BANDITS;
+	private ArrayList<Marshall> MARSHALLS;
+	private Case[][] train;
+	private ArrayList<Tuple<Action,Integer>> todo;
+  
+  public Train(int nb_wagons, ArrayList<Bandit> bandits, ArrayList<Marshall> marshalls) {
+  }
