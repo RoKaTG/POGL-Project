93107a95c047d26adb0dc69286c24107e79f3b77
ROKATG0D <58750536+ROKATG0D@users.noreply.github.com>
2021-05-04 14:18:26 +0200

    Update Marshall

--- Marshall.java
+++ Marshall.java
@@ -1,1 +1,14 @@
+import java.util.Random;
 
+public class Marshall {       //J'ai besoin d'une nouvelle classe pour les cara des persnnages svp.
+
+  private String name;
+	private final double Nervo_Marshall;
+	private Random rand;
+	private int ID;
+}
+
+/*public Marshall() {
+}*/
+
+/*/Besoin de methods de deplacement ?/*/
