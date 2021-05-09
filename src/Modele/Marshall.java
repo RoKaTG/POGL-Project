36d1be933af1259e4960300732b484328804284f
Yassine <msiliniyassine@gmail.com>
2021-05-09 14:13:18 +0200

    Update Constructeurs et Attributs.

--- src/Modele/Marshall.java
+++ src/Modele/Marshall.java
@@ -3,5 +3,13 @@ package Modele;
 import java.util.Random;
 
 public class Marshall {
-   int i;
+    public double NERVOSITE_MARSHALL;
+    protected int nWagon;
+    protected boolean niveau;
+
+    public Marshall(double NERVOSITE_MARSHALL) {
+        this.NERVOSITE_MARSHALL = NERVOSITE_MARSHALL;
+        this.nWagon = Train.NB_WAGON - 1;
+        this.niveau = false;
+    }
 }
