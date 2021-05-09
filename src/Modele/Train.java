3d58e1a1b99352c46926d45288be708659fc2472
Yassine <msiliniyassine@gmail.com>
2021-05-09 13:03:16 +0200

    Reprise au propre de Train.java

---
+++ src/Modele/Train.java
@@ -0,0 +1,15 @@
+package Modele;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.UIManager;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+
+public class Train {
+    public static int NB_WAGONN;
+    public static int NB_BANDIT = 1;
+    public static final int NB_ACTION = 4;
+    public static Marshall marhsall;
+}
