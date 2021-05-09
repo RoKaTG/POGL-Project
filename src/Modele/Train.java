af25f26d6854ba71d33b56fd2a7736f9bcb57240
Yassine <msiliniyassine@gmail.com>
2021-05-09 14:51:22 +0200

    Update Tain.java + Constructeur

--- src/Modele/Train.java
+++ src/Modele/Train.java
@@ -1,15 +1,21 @@
 package Modele;
 
 import java.util.ArrayList;
-import java.util.Scanner;
-
-import javax.swing.UIManager;
-import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 
 public class Train {
-    public static int NB_WAGONN;
+    public static int NB_WAGON;
     public static int NB_BANDIT = 1;
     public static final int NB_ACTION = 4;
-    public static Marshall marhsall;
+    public static int NB_MANCHE;
+    public static Marshall marshall;
+    private String N;
+    ArrayList<Wagon> wagonList = new ArrayList<Wagon();
+    ArrayList<Bandit> banditList = new ArrayList<Bandit>();
+
+    public Train(int nWagon) {
+        Train.NB_WAGON = nWagon;
+        Train.NB_MANCHE = 1;
+        Train.marshall = new Marshall(0.3);
+    }
 }
