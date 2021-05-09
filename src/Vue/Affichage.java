a7aa25a022f433a386af6a41cbb8ed703ccc6f3f
Yassine <msiliniyassine@gmail.com>
2021-05-09 18:34:07 +0200

    Commencement Affichage.java

--- src/Vue/Affichage.java
+++ src/Vue/Affichage.java
@@ -1,4 +1,22 @@
 package Vue;
 
-public class Affichage {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Font;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class Affichage extends JFrame {
+    private final Color title = new Color(255,255,255);
+    private final Color infoT = Color.white;
 }
