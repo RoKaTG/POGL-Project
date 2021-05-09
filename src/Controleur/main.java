6600d32b6752476c45d08d154ca63a657512eb13
Yassine <msiliniyassine@gmail.com>
2021-05-09 18:33:51 +0200

    Classe complète

--- src/Controleur/main.java
+++ src/Controleur/main.java
@@ -1,5 +1,26 @@
 package Controleur;
 
+import Modele.Train;
+import Vue.Affichage;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.UIManager;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
 public class main {
-    int a = 0;
+    public static void main(String[] args) throws Exception {
+        UIManager.setLookAndFeel(new NimbusLookAndFeel());
+
+        Train plateau = new Train(0);
+        plateau.trainInitialise();
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                Affichage f = new Affichage();
+                f.setVisible(true);
+            }
+        });
+    }
 }
